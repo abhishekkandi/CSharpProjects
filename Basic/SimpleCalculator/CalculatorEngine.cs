afbe10b469b4b03b76f4170d0a9d1c24c9961329
abhishekkandi <kandii.abhishek@gmail.com>
2019-07-21 22:03:16 +0530

    Simple calculator application

---
+++ Basic/SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class CalculatorEngine
+    {
+        internal double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized.");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Basic/SimpleCalculator/InputConvertor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class InputConvertor
+    {
+        internal double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber))
+                throw new ArgumentException("Expected a numeric value.");
+
+            return convertedNumber;
+        }
+    }
+}
