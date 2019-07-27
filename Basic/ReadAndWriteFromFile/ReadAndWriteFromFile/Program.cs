64886b7355f5e9023f07739721a7fa1f05613592
abhishekkandi <kandii.abhishek@gmail.com>
2019-07-27 20:53:00 +0530

    C# project demonstration simple file operations i.e, Reading and Writing into a file.

---
+++ Basic/ReadAndWriteFromFile/ReadAndWriteFromFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAndWriteFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = {
+                                "This is the first line",
+                                "This is the second line",
+                                "This is the third line",
+                             };
+
+            File.WriteAllLines("MyFirstFile.txt", lines);
+
+            string[] fileContents = File.ReadAllLines("MyFirstFile.txt");
+
+            foreach(string line in File.ReadLines("MyFirstFile.txt"))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
