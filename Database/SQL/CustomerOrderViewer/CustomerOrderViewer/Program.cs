1c621c579ce1c78acc37817c5ea69da1173cdb41
abhishekkandi <kandii.abhishek@gmail.com>
2020-03-25 20:32:59 +0530

    Customer order viewer project initial setup

---
+++ Database/SQL/CustomerOrderViewer/CustomerOrderViewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
