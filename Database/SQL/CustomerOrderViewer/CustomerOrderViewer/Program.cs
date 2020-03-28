b57a2018f605ff941a2384b2ac19245bb480889b
abhishekkandi <kandii.abhishek@gmail.com>
2020-03-29 00:34:50 +0530

    CustomerOrderViewer project

---
+++ Database/SQL/CustomerOrderViewer/CustomerOrderViewer/Program.cs
@@ -0,0 +1,34 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand("Data Source=localhost;Initial Catalog=CustomerOrderViewer;Integrated Security=True");
+
+                IList<CustomerOrderDetailModel> customerOrders = customerOrderDetailCommand.GetList();
+
+                if (customerOrders.Any())
+                {
+                    foreach(var customerOrder in customerOrders)
+                    {
+                        Console.WriteLine($"{customerOrder.CustomerOrderId}: FullName: {customerOrder.FirstName} {customerOrder.LastName} (CustomerId: {customerOrder.CustomerId}) - purchased {customerOrder.Description} for {customerOrder.Price} (ItemId: {customerOrder.ItemId})");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Something went wrong: {ex.Message}");
+            }
+            
+        }
+    }
+}
