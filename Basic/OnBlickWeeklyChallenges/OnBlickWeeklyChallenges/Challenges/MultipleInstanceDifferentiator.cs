1ce24a301daa56247e45b51d897809bf94b0e044
abhishekkandi <kandii.abhishek@gmail.com>
2020-04-19 15:33:38 +0530

    OnBlickWeeklyChallenges - MultipleInstanceDifferentiator (.NET Core console application)

---
+++ Basic/OnBlickWeeklyChallenges/OnBlickWeeklyChallenges/Challenges/MultipleInstanceDifferentiator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnBlickWeeklyChallenges
+{
+    public static class MultipleInstanceDifferentiator
+    {
+        public static void Run()
+        {
+            Console.WriteLine($"{Environment.NewLine}Started MultipleInstanceDifferentiator: ");
+
+            var employee1 = new Employee
+            {
+                HiredDate = new DateTime(2018, 03, 22),
+                Deparment = new Deparment { Name = "Engineering" },
+                Email = "abhishek@xyz.com",
+                Name = "Abhishek",
+                Organizaion = new Organizaion { Name = "XYZ" }
+            };
+
+            var employee2 = new Employee
+            {
+                HiredDate = new DateTime(2018, 03, 28),
+                Deparment = new Deparment { Name = "DevOps" },
+                Email = "akshay@abc.com",
+                Name = "Akshay",
+                Organizaion = new Organizaion { Name = "ABC" }
+            };
+
+            var differences = employee1.GetDifference(employee2);
+
+            Console.WriteLine(differences.ToString());
+
+        }
+
+        public static StringBuilder GetDifference<T>(this T instance1, T instance2, string instancePath = "")
+        {
+            var differences = new StringBuilder();
+
+            var instanceType = instance1.GetType();
+            instancePath = !string.IsNullOrEmpty(instancePath) ? instancePath + "." + instanceType.Name : instanceType.Name;
+
+            foreach (var instanceProperty in instanceType.GetProperties())
+            {
+                if (!instanceProperty.CanWrite)
+                    continue;
+
+                var typeOfProperty = instanceProperty.PropertyType;
+                var instance1PropertyValue = instanceProperty.GetValue(instance1);
+                var instance2PropertyValue = instanceProperty.GetValue(instance2);
+
+                if (instance1PropertyValue != null && instance2PropertyValue != null)
+                {
+                    if (typeOfProperty.IsClass && !typeOfProperty.Namespace.Equals("System"))
+                    {
+                        var subInstanceDifferences = instance1PropertyValue.GetDifference(instance2PropertyValue, instancePath);
+                        differences.Append(subInstanceDifferences.ToString());
+                    }
+                    else
+                    {
+                        if (!instance1PropertyValue.Equals(instance2PropertyValue))
+                        {
+                            differences.Append($"{instancePath}.{instanceProperty.Name}: Instance-1 value: {instance1PropertyValue}; Instance-2 value: {instance2PropertyValue} {Environment.NewLine}");
+                        }
+                    }
+                }
+            }
+
+            return differences;
+        }
+    }
+
+    public class Organizaion
+    {
+        public string Name { get; set; }
+    }
+
+    public class Deparment
+    {
+        public string Name { get; set; }
+    }
+
+    public class Employee
+    {
+        public DateTime HiredDate { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Organizaion Organizaion { get; set; }
+        public Deparment Deparment { get; set; }
+    }
+
+    
+}
---
+++ Basic/OnBlickWeeklyChallenges/OnBlickWeeklyChallenges/Program.cs
@@ -0,0 +1,34 @@
+﻿using OnBlickWeeklyChallenges;
+using System;
+using System.Text;
+
+namespace OnBlickWeeklyChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select your option: ");
+            
+            var options = new StringBuilder();
+            options.Append($"1. Multiple Instance Differentiator {Environment.NewLine}");
+
+            Console.WriteLine(options.ToString());
+
+            string selectedOption = Console.ReadLine();
+            int choice = 0;
+
+            if(int.TryParse(selectedOption,out choice)) 
+            {
+                switch (choice)
+                {
+                    case 1:
+                        MultipleInstanceDifferentiator.Run();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
